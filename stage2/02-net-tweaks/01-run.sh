92a91903a09f28cd615f19bb5a98b695e2ffce96
Zach Boyd <zachboyd@users.noreply.github.com>
2019-05-01 18:37:08 -0500

    Update 01-run.sh

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -15,3 +15,6 @@ on_chroot <<EOF
 wpa_passphrase "${WPA_ESSID}" "${WPA_PASSWORD}" >> "/etc/wpa_supplicant/wpa_supplicant.conf"
 EOF
 fi
+
+echo "\ninterface eth0\nstatic ip_address=192.168.1.50/24\nstatic routers=192.168.1.1\nstatic domain_name_servers=192.168.1.1 8.8.8.8" >> "${ROOTFS_DIR}/etc/dhcpcd.conf"
+echo "net.ipv6.conf.all.disable_ipv6 = 1" > "${ROOTFS_DIR}/etc/sysctl.d/local.conf"
